1ae99d89b785be420fd0baabc97ccc9d10b4814e
Valentina Zantedeschi <vzantedeschi@gmail.com>
2018-01-09 11:30:39 +0100

    run on ijcnn1 dataset

--- src/main.py
+++ src/main.py
@@ -1,37 +1,47 @@
 from copy import deepcopy
 import numpy as np
 
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.utils import shuffle
 
-from evaluation import alpha_variance, loss, mean_accuracy
+from classification import RandomClassifier
+from evaluation import alpha_variance, central_loss, central_accuracy
 from network import complete_graph, synthetic_graph
 from optimization import average_FW, local_FW, neighbor_FW, centralized_FW
-from utils import load_breast_dataset, generate_models, generate_samples
+from utils import load_dense_dataset, load_breast_dataset, generate_models, generate_samples
 
 NB_ITER = 100
-N = 2
-# D = 10
-# random_state = 20160922
-# V, theta_true, cluster_indexes = generate_models(nb_clust=1, nodes_per_clust=N, random_state=random_state)
-# _, X, Y, X_test, Y_test, _, _ = generate_samples(V, theta_true, D, random_state=random_state)
-# set graph
-# nodes = synthetic_graph(X, Y, X_test, Y_test, V, theta_true)
+N = 10
 
-X, Y = load_breast_dataset()
-M, D = X.shape
-nodes = complete_graph(X, Y, nb_nodes=N, cluster_data=False)
+TRAIN_FILE = "datasets/ijcnn1.train"
+TEST_FILE = "datasets/ijcnn1.test"
+
+train_x, train_y = load_dense_dataset(TRAIN_FILE)
+test_x, test_y = load_dense_dataset(TEST_FILE)
+D = test_x.shape[1]
+
+# train_x, train_y = load_breast_dataset()
+
+nodes = complete_graph(train_x, train_y, nb_nodes=N, cluster_data=False)
+
+# set test set
+test_m = test_x.shape[0]
+test_x_copy = np.c_[test_x, np.ones(test_m)]
+for n in nodes:
+    n.set_test_set(test_x_copy, test_y)
 
 # set callbacks for optimization analysis
 callbacks = {
-    'mean-accuracy': [mean_accuracy, []],
-    'train-loss': [loss, []]
+    'accuracy': [central_accuracy, []],
+    'train-loss': [central_loss, []]
 }
 
 methods = {
     "centralized": centralized_FW, 
     "local": local_FW,
-    "average": average_FW,
-    "neighbor": neighbor_FW
+    "average": average_FW
 }
 
 results = {}
@@ -39,7 +49,15 @@ for k, m in methods.items():
 
     nodes_copy = deepcopy(nodes)
     results[k] = m(nodes_copy, D, NB_ITER, callbacks=callbacks)
-    print(len(nodes_copy))
+
+bdt = AdaBoostClassifier(DecisionTreeClassifier(max_depth=1), algorithm="SAMME", n_estimators=NB_ITER)
+bdt.fit(train_x, train_y)
+print(accuracy_score(bdt.predict(train_x), train_y))
+print(accuracy_score(bdt.predict(test_x), test_y))
+
+random_clf = RandomClassifier()
+train_acc_rnd = random_clf.score(train_x, train_y)
+test_acc_rnd = random_clf.score(test_x, test_y)
 
 import matplotlib.pyplot as plt
 
@@ -47,9 +65,11 @@ plt.figure(1)
 plt.xlabel('nb iterations')
 plt.ylabel('train accuracy')
 
-for k, r_list in results.items():
-    plt.plot(range(NB_ITER), [r['mean-accuracy'][0] for r in r_list], label='{}'.format(k))
+x_len = len(results["centralized"])
 
+for k, r_list in results.items():
+    plt.plot(range(x_len), [r['accuracy'][0] for r in r_list], label='{}'.format(k))
+plt.plot(range(x_len), [train_acc_rnd]*x_len, label='random')
 plt.legend()
 
 plt.figure(2)
@@ -57,8 +77,8 @@ plt.xlabel('nb iterations')
 plt.ylabel('test accuracy')
 
 for k, r_list in results.items():
-    plt.plot(range(NB_ITER), [r['mean-accuracy'][1] for r in r_list], label='{}'.format(k))
-
+    plt.plot(range(x_len), [r['accuracy'][1] for r in r_list], label='{}'.format(k))
+plt.plot(range(x_len), [test_acc_rnd]*x_len, label='random')
 plt.legend()
 
 plt.figure(3)
@@ -66,7 +86,7 @@ plt.xlabel('nb iterations')
 plt.ylabel('train loss')
 
 for k, r_list in results.items():
-    plt.plot(range(NB_ITER), [r['train-loss'] for r in r_list], label='{}'.format(k))
+    plt.plot(range(x_len), [r['train-loss'] for r in r_list], label='{}'.format(k))
 
 plt.legend()
 

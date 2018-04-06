6bcb2f5c6fffac1e4160e7bb3f0750610fea2ac0
vzantedeschi <vzantedeschi@gmail.com>
2018-04-06 12:46:14 +0200

    no need for callbacks in tuning

--- src/tuning_lafond_moons.py
+++ src/tuning_lafond_moons.py
@@ -6,11 +6,11 @@ from statistics import mean
 from classification import get_stumps
 from evaluation import central_accuracy
 from network import synthetic_graph
-from optimization import gd_reg_local_FW
+from related_works import lafond_FW
 from utils import generate_models, generate_moons, get_split_per_list, get_min_max
 
 # set graph of nodes with local personalized data
-NB_ITER = 200
+NB_ITER = 100
 N = 100
 D = 20
 B = 200
@@ -18,15 +18,13 @@ NOISE_R = 0.05
 random_state = 2017
 
 CV_SPLITS = 3
-MU_LIST = [10**i for i in range(-3, 4)]
 BETA_LIST = [10**i for i in range(5)]
-STEP_LIST = list(range(10, 110, 10))
 
 V, theta_true, cluster_indexes = generate_models(nb_clust=1, nodes_per_clust=N, random_state=random_state)
 _, X, Y, _, _, _, _ = generate_moons(V, theta_true, D, random_state=random_state, sample_error_rate=NOISE_R)
 
 results = {}
-results = results.fromkeys(itertools.product(MU_LIST, BETA_LIST, STEP_LIST), 0.)
+results = results.fromkeys(BETA_LIST, 0.)
 
 for indices in get_split_per_list(X, CV_SPLITS, rnd_state=random_state):
 
@@ -44,20 +42,13 @@ for indices in get_split_per_list(X, CV_SPLITS, rnd_state=random_state):
     # set graph
     nodes, _, _ = synthetic_graph(train_x, train_y, test_x, test_y, V, theta_true)
 
-    for mu in MU_LIST:
+    for beta in BETA_LIST:
 
-        for beta in BETA_LIST:
+        print(beta)
+        nodes_copy = deepcopy(nodes)
+        lafond_FW(nodes_copy, base_clfs, nb_iter=NB_ITER, beta=beta, callbacks={})
 
-            for step in STEP_LIST:
-
-
-                print(mu, beta, step)
-
-                nodes_copy = deepcopy(nodes)
-                r = gd_reg_local_FW(nodes_copy, base_clfs, gd_method={"name":"laplacian", "pace_gd": step, "args":()}, beta=beta, mu=mu, nb_iter=NB_ITER, reset_step=False, callbacks={})
-
-                # keep value of last iteration
-                results[(mu, beta, step)] += central_accuracy(nodes_copy)[1]
+        results[beta] += central_accuracy(nodes_copy)[1]
 
 
-print("best mu, beta, step:", max(results, key=results.get))
+print("best beta:", max(results, key=results.get))
--- src/tuning_regularized_moons.py
+++ src/tuning_regularized_moons.py
@@ -11,9 +11,9 @@ from utils import generate_models, generate_moons, get_split_per_list, get_min_m
 
 # set graph of nodes with local personalized data
 NB_ITER = 100
-N = 100
+N = 20
 D = 20
-B = 100
+B = 200
 NOISE_R = 0.05
 random_state = 2017
 
@@ -23,11 +23,6 @@ BETA_LIST = [10**i for i in range(5)]
 
 V, theta_true, cluster_indexes = generate_models(nb_clust=1, nodes_per_clust=N, random_state=random_state)
 _, X, Y, _, _, _, _ = generate_moons(V, theta_true, D, random_state=random_state, sample_error_rate=NOISE_R)
-
-# set callbacks for optimization analysis
-callbacks = {
-    'accuracy': [central_accuracy, []]
-}
 
 results = {}
 results = results.fromkeys(itertools.product(MU_LIST, BETA_LIST), 0.)
@@ -54,10 +49,9 @@ for indices in get_split_per_list(X, CV_SPLITS, rnd_state=random_state):
 
             print(mu, beta)
             nodes_copy = deepcopy(nodes)
-            r = regularized_local_FW(nodes_copy, base_clfs, nb_iter=NB_ITER, beta=beta, mu=mu, callbacks=callbacks)
+            r = regularized_local_FW(nodes_copy, base_clfs, nb_iter=NB_ITER, beta=beta, mu=mu, callbacks={})
 
-            # keep value of last iteration
-            results[(mu, beta)] += r[NB_ITER]["accuracy"][1]
+            results[(mu, beta)] += central_accuracy(nodes_copy)[1]
 
 
 print("best mu, beta:", max(results, key=results.get))

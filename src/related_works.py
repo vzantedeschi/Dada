aab1685c099d0429dca708ed421464cdeef88ac4
Valentina Zantedeschi <vzantedeschi@gmail.com>
2018-04-18 14:29:52 +0200

    split train test accuracy monitor

--- src/related_works.py
+++ src/related_works.py
@@ -139,20 +139,20 @@ def compute_graph_matrices(n, adjacency, similarities):
 
     return L, d
 
-def colearning(nb_nodes, x, y, x_test, y_test, dim, nb_iter, adjacency, similarities, max_samples_per_node=20, checkevery=1):
+def colearning(nb_nodes, x, y, x_test, y_test, dim, nb_iter, adjacency, similarities, mu=1, max_samples_per_node=20, checkevery=1):
 
     results = []
-    alpha = 0.5
+    alpha = 1 / (1 + mu)
     L, d = compute_graph_matrices(nb_nodes, adjacency, similarities)
 
     theta = np.zeros((nb_nodes, dim))
-    results.append({"accuracy": (class_ratio(theta, x, y), class_ratio(theta, x_test, y_test))})
+    results.append({"train-accuracy": class_ratio(theta, x, y), "test-accuracy": class_ratio(theta, x_test, y_test)})
     
     # Collaborative learning
     for t in range(nb_iter):
         theta -= cost_function_gradient(L, d, theta, x, y, alpha, max_samples_per_node)
 
         if t % checkevery == 0:
-            results.append({"accuracy": (class_ratio(theta, x, y), class_ratio(theta, x_test, y_test))})
+            results.append({"train-accuracy": class_ratio(theta, x, y), "test-accuracy": class_ratio(theta, x_test, y_test)})
 
     return results, theta

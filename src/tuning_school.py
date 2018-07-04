2fe0e0a2f68155705ddde3c55ff115209b350259
Valentina Zantedeschi <vzantedeschi@gmail.com>
2018-07-04 11:12:31 +0200

    estimate school graph. average over years

--- src/tuning_school.py
+++ src/tuning_school.py
@@ -4,9 +4,9 @@ import numpy as np
 from statistics import mean
 
 from classification import get_stumps
-from evaluation import central_test_accuracy
-from network import null_graph
-from optimization import gd_reg_local_FW
+from evaluation import central_test_accuracy, edges
+from network import null_graph, graph
+from optimization import gd_reg_local_FW, regularized_local_FW
 from utils import load_school, get_split_per_list, get_min_max
 
 # set graph of nodes with local personalized data
@@ -17,11 +17,11 @@ random_state = 2018
 CV_SPLITS = 3
 MU_LIST = [10**i for i in range(-3, 4)]
 BETA_LIST = [10**i for i in range(3)]
-STEP = 50
+# STEP = 50
 # MU_LIST = [0.1]
 # BETA_LIST = [1]
 
-X, Y, _, _, N, max_nb_instances = load_school(thr=20)
+X, Y, _, _, adjacency, distances, N, max_nb_instances = load_school(thr=20)
 D = X[0].shape[1]
 
 results = {}.fromkeys(itertools.product(MU_LIST, BETA_LIST), 0.)
@@ -40,7 +40,8 @@ for indices in get_split_per_list(X, CV_SPLITS, rnd_state=random_state):
     base_clfs = get_stumps(n=B, d=D, min_v=vmin, max_v=vmax)
 
     # get nodes
-    nodes = null_graph(train_x, train_y, test_x, test_y, N, max_nb_instances)
+    # nodes = null_graph(train_x, train_y, test_x, test_y, N, max_nb_instances)
+    nodes = graph(train_x, train_y, test_x, test_y, N, adjacency, distances, max_nb_instances)
 
     for mu in MU_LIST:
 
@@ -50,9 +51,12 @@ for indices in get_split_per_list(X, CV_SPLITS, rnd_state=random_state):
 
             init_w = np.eye(N)
             nodes_copy = deepcopy(nodes)
-            gd_reg_local_FW(nodes_copy, base_clfs, init_w, gd_method={"name":"laplacian", "pace_gd": STEP, "args":(8)}, beta=beta, mu=mu, nb_iter=NB_ITER, reset_step=False, monitors={})
+            # gd_reg_local_FW(nodes_copy, base_clfs, init_w, gd_method={"name":"laplacian", "pace_gd": STEP, "args":(8)}, beta=beta, mu=mu, nb_iter=NB_ITER, reset_step=False, monitors={})
+            regularized_local_FW(nodes_copy, base_clfs, nb_iter=NB_ITER, beta=beta, mu=mu, monitors={})
 
             results[(mu, beta)] += central_test_accuracy(nodes_copy)
 
+# print(edges(nodes_copy))
+# print(results)
 
 print("best mu, beta:", max(results, key=results.get))

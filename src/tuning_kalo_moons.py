21fc4a913fa127188f618639dc6a5429adbc1afe
Valentina Zantedeschi <vzantedeschi@gmail.com>
2018-10-23 15:15:27 +0200

    misc

--- src/tuning_kalo_moons.py
+++ src/tuning_kalo_moons.py
@@ -10,8 +10,8 @@ from optimization import gd_reg_local_FW
 from utils import generate_models, generate_moons, get_split_per_list, get_min_max, kalo_utils
 
 # set graph of nodes with local personalized data
-NB_ITER = 3000 # 10000 for 100 nodes
-K = 20 # 100 for 100 nodes
+NB_ITER = 10000 # for 100 nodes
+K = 100 # 100 for 100 nodes
 STEP = 300 # 500 for 100 nodes
 
 D = 20
@@ -21,13 +21,15 @@ random_state = 2018
 BETA = 10
 
 ITER = 3
-MU_LIST = [10**i for i in range(-3, 3)]
-LA_LIST = [10**i for i in range(-3, 3)]
+# MU_LIST = [10**i for i in range(-3, 3)]
+# LA_LIST = [10**i for i in range(-3, 3)]
+MU_LIST = [10**i for i in range(1)]
+LA_LIST = [10**i for i in range(1)]
 # NU_LIST = [10**i for i in range(-3, 3)]
 
 results = {}.fromkeys(itertools.product(MU_LIST, LA_LIST), 0.)
 
-for i in range(2, ITER+2):
+for i in range(2):
     _, theta_true, _ = generate_models(nb_clust=1, nodes_per_clust=K, random_state=random_state * i)
     _, train_x, train_y, test_x, test_y, max_nb_instances = generate_moons(K, theta_true, D, random_state=random_state * i, sample_error_rate=NOISE_R)
 
